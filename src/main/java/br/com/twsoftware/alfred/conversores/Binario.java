6d042a39b7351700cd7c81e370a2f7f44728c562
Thiago Sampaio <thiago.sampaio@conductor.com.br>
2018-04-03 10:24:31 -0300

    Tornando o código compilável no JDK 10

--- src/main/java/br/com/twsoftware/alfred/conversores/Binario.java
+++ src/main/java/br/com/twsoftware/alfred/conversores/Binario.java
@@ -14,12 +14,10 @@
  *  You should have received a copy of the GNU General Public License
  *  along with Alfred Library.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package br.com.twsoftware.alfred.conversores;
 
-import java.io.IOException;
-
-import br.com.twsoftware.alfred.AlfredException;
-
+import java.util.Base64;
 
 /**
  * Conversor de tipos bin�rios.
@@ -27,30 +25,31 @@ import br.com.twsoftware.alfred.AlfredException;
  * @author Marlon Silva Carvalho
  * @since 11/06/2009
  */
-final public class Binario {
+final public class Binario{
 
-	/**
-	 * Converter um array de bytes em uma representa��o em Base 64.
-	 * 
-	 * @param bytes Bytes.
-	 * @return String contendo a representa��o em Base 64.
-	 */
-	public static String codificarBase64(byte[] bytes) {
-		return new sun.misc.BASE64Encoder().encode(bytes);
-	}
+     /**
+      * Converter um array de bytes em uma representa��o em Base 64.
+      * 
+      * @param bytes
+      * Bytes.
+      * @return String contendo a representa��o em Base 64.
+      */
+     public static String codificarBase64(byte[] bytes) {
+          
+          return Base64.getEncoder().encodeToString(bytes);
+          
+     }
 
-	/**
-	 * Decodificar uma String em Base64 para bin�rio.
-	 * 
-	 * @param base Base64.
-	 * @return Bin�rio.
-	 */
-	public static byte[] decodificarBase64(String base) {
-		try {
-			return new sun.misc.BASE64Decoder().decodeBuffer(base);
-		} catch (IOException e) {
-			throw new AlfredException(e);
-		}
-	}
+     /**
+      * Decodificar uma String em Base64 para bin�rio.
+      * 
+      * @param base
+      * Base64.
+      * @return Bin�rio.
+      */
+     public static byte[] decodificarBase64(String base) {
+
+          return Base64.getDecoder().decode(base);
+     }
 
 }
